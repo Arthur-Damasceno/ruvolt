9e45a5f5eb14a7a121bad0057ec8abeb89bd3c96
Arthur Damasceno <damascenoarthur653@gmail.com>
2022-01-24 09:37:39 -0300

    setup

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#![deny(unsafe_code)]
+#![warn(
+    missing_debug_implementations,
+    missing_copy_implementations,
+    missing_docs,
+    clippy::missing_panics_doc
+)]
+
+//! # [Ruvolt](crate)
+//! ## An API wrapper for Revolt written in Rust.
