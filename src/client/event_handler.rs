84fdedd95d5a9ae3a3cafb14f435d68a54ba0983
Arthur Damasceno <damascenoarthur653@gmail.com>
2022-01-25 19:44:13 -0300

    internal(client): `EventHandlerExt`

--- src/client/event_handler.rs
+++ src/client/event_handler.rs
@@ -1,6 +1,6 @@
 use async_trait::async_trait;
 
-use crate::error::Error;
+use crate::{entities::ServerToClientEvent, error::Error};
 
 /// Define handlers for supported events.
 #[async_trait]
@@ -8,3 +8,17 @@ pub trait EventHandler: Send + Sync + 'static {
     /// An error has occurred.
     async fn error(&self, error: Error);
 }
+
+#[async_trait]
+pub(crate) trait EventHandlerExt: EventHandler {
+    async fn handle(&self, event: ServerToClientEvent) {
+        match event {
+            event => {
+                let error = Error::Unknown(format!("Unexpected event from server: {:?}", event));
+                self.error(error).await;
+            }
+        }
+    }
+}
+
+impl<T: EventHandler> EventHandlerExt for T {}
