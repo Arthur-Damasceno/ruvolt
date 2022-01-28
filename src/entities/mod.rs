b382b112578987b56882f3a571c01c0308c026c0
Arthur Damasceno <damascenoarthur653@gmail.com>
2022-01-28 12:20:13 -0300

    feat(entities): `Server`

--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -1,9 +1,10 @@
 //! Module for all entities like [ClientToServerEvent], [Message], server members, etc.
 
 #[doc(inline)]
-pub use {channel::*, events::*, message::*, user::*};
+pub use {channel::*, events::*, message::*, server::*, user::*};
 
 mod channel;
 mod events;
 mod message;
+mod server;
 mod user;
---
+++ src/entities/server/mod.rs
@@ -0,0 +1,25 @@
+use serde::Deserialize;
+
+/// A server.
+#[derive(Debug, Deserialize, Clone, PartialEq)]
+pub struct Server {
+    /// Server id.
+    #[serde(rename = "_id")]
+    pub id: String,
+    /// Server owner id.
+    #[serde(rename = "owner")]
+    pub owner_id: String,
+    /// Server name.
+    pub name: String,
+    /// Server description.
+    #[serde(default)]
+    pub description: Option<String>,
+    /// Server channels ids.
+    pub channels: Vec<String>,
+    /// Server flags.
+    #[serde(default)]
+    pub flags: Option<u32>,
+    /// Server is not safe for work.
+    #[serde(default)]
+    pub nsfw: bool,
+}
