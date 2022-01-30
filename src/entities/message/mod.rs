0d9e9c57e954aca3214f5f11a457d0dfddce7b9a
Arthur Damasceno <damascenoarthur653@gmail.com>
2022-01-29 22:20:33 -0300

    feat(entities): reply messsages

--- src/entities/message/mod.rs
+++ src/entities/message/mod.rs
@@ -3,7 +3,7 @@ pub use content::*;
 mod content;
 mod edited;
 
-use serde::Deserialize;
+use {serde::Deserialize, serde_json::json};
 
 use {
     crate::{
@@ -75,5 +75,27 @@ impl Message {
     /// Get the message author from the API.
     pub async fn fetch_author(&self, cx: &Context) -> Result<User> {
         User::fetch(cx, &self.author_id).await
+    }
+
+    /// Reply the message.
+    pub async fn reply(&self, cx: &Context, content: &str) -> Result<Self> {
+        let response = cx
+            .http_client
+            .post(format!(
+                "{}channels/{}/messages",
+                REVOLT_API, &self.channel_id
+            ))
+            .json(&json!({
+                "content": content,
+                "replies": [{
+                    "id": self.id,
+                    "mention": true,
+                }]
+            }))
+            .send()
+            .await?;
+        let msg = response.json().await?;
+
+        Ok(msg)
     }
 }
