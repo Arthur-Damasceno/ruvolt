0a3c874d26cf9c1a0be7ce581b7926f229a4bbaf
Arthur Damasceno <damascenoarthur653@gmail.com>
2022-01-31 16:24:30 -0300

    feat(http): make a GET requests with `HttpClient`

--- src/client/http.rs
+++ src/client/http.rs
@@ -1,9 +1,17 @@
 //! A module for the http client that makes requests to the Revolt REST API.
 
-use reqwest::{
-    header::{HeaderMap, HeaderValue},
-    Client,
+use {
+    reqwest::{
+        header::{HeaderMap, HeaderValue},
+        Client,
+    },
+    serde::de::DeserializeOwned,
 };
+
+use crate::Result;
+
+/// The url of the Revolt REST API.
+pub const REVOLT_API: &str = "https://api.revolt.chat";
 
 /// A struct to execute requests to the [Revolt REST API](https://developers.revolt.chat/api/).
 #[derive(Debug, Clone)]
@@ -17,5 +25,14 @@ impl HttpClient {
         let client = Client::builder().default_headers(headers).build().unwrap();
 
         Self(client)
+    }
+
+    /// Make a GET request to the API and convert the body to json.
+    pub async fn get<T: DeserializeOwned>(&self, path: &str) -> Result<T> {
+        let url = format!("{}/{}", REVOLT_API, path);
+        let response = self.0.get(url).send().await?;
+        let body = response.json().await?;
+
+        Ok(body)
     }
 }
