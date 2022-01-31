7eeee0f0822efddef52ad8a992c203d51180e2af
Arthur Damasceno <damascenoarthur653@gmail.com>
2022-01-31 18:07:22 -0300

    feat(entities): send messages in a channel

--- src/entities/channel/group.rs
+++ src/entities/channel/group.rs
@@ -1,4 +1,4 @@
-use serde::Deserialize;
+use {serde::Deserialize, serde_json::json};
 
 use crate::{
     entities::{Message, User},
@@ -41,5 +41,14 @@ impl GroupChannel {
             }
             None => Ok(None),
         }
+    }
+
+    /// Send a message in this channel.
+    pub async fn send(&self, cx: &Context, content: &str) -> Result<Message> {
+        let path = format!("channels/{}/messages", self.id);
+        let body = json!({ "content": content });
+        let msg = cx.http_client.post(&path, body).await?;
+
+        Ok(msg)
     }
 }
