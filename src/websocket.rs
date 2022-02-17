72089d2dfff41afc10e53a828cf4e1a47878d336
Arthur Damasceno <damascenoarthur653@gmail.com>
2022-02-17 17:17:31 -0300

    feat(websocket): accept incoming events

--- src/websocket.rs
+++ src/websocket.rs
@@ -1,9 +1,10 @@
 use {
+    futures_util::StreamExt,
     tokio::net::TcpStream,
-    tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream},
+    tokio_tungstenite::{connect_async, tungstenite::Message, MaybeTlsStream, WebSocketStream},
 };
 
-use crate::Result;
+use crate::{error::Error, models::events::ServerToClientEvent, Result};
 
 const REVOLT_WS_API: &str = "wss://ws.revolt.chat";
 
@@ -17,5 +18,28 @@ impl WebSocketClient {
         let (stream, _) = connect_async(REVOLT_WS_API).await?;
 
         Ok(Self { stream })
+    }
+
+    pub async fn accept(&mut self) -> Option<Result<ServerToClientEvent>> {
+        match self.stream.next().await? {
+            Ok(msg) => match msg {
+                Message::Text(text) => {
+                    let event = serde_json::from_str(&text).map_err(|_| {
+                        Error::Unknown(format!(
+                            "Cannot deserialize a websocket message: {:?}",
+                            text
+                        ))
+                    });
+
+                    match event {
+                        Ok(event) => Some(Ok(event)),
+                        Err(err) => Some(Err(err)),
+                    }
+                }
+                Message::Close(_) => None,
+                _ => todo!(),
+            },
+            Err(err) => Some(Err(err.into())),
+        }
     }
 }
