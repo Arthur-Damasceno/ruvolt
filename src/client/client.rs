8b1de7c1232ac06acfc6fa5ae6a57685f8f20370
Arthur Damasceno <damascenoarthur653@gmail.com>
2022-02-21 22:02:47 -0300

    feat(client): store token in `Context` and remove useless `Arc`

--- src/client/client.rs
+++ src/client/client.rs
@@ -2,7 +2,6 @@ use std::sync::Arc;
 
 use crate::{
     error::Error,
-    http::HttpClient,
     models::events::{ClientToServerEvent, ServerToClientEvent},
     websocket::WebSocketClient,
     Context, EventHandler, EventHandlerExt, Result,
@@ -13,30 +12,25 @@ use crate::{
 pub struct Client<T: EventHandler> {
     event_handler: Arc<T>,
     ws_client: WebSocketClient,
-    http_client: Arc<HttpClient>,
-    token: String,
+    context: Context,
 }
 
 impl<T: EventHandler> Client<T> {
     /// Create a new client and connect to the server.
     pub async fn new(event_handler: T, token: impl Into<String>) -> Result<Self> {
         let ws_client = WebSocketClient::connect().await?;
-        let token = token.into();
-        let http_client = Arc::new(HttpClient::new(&token));
+        let context = Context::new(token);
 
         Ok(Self {
             event_handler: Arc::new(event_handler),
-            http_client,
-            token,
             ws_client,
+            context,
         })
     }
 
     /// Start listening for server events.
     pub async fn listen(&mut self) -> Result {
         self.authenticate().await?;
-
-        let cx = Context::new(self.http_client.clone());
 
         loop {
             if let Err(err) = self.ws_client.check_heartbeat().await {
@@ -45,11 +39,11 @@ impl<T: EventHandler> Client<T> {
 
             if let Some(event) = self.ws_client.accept().await {
                 let event_handler = self.event_handler.clone();
-                let cx = cx.clone();
+                let context = self.context.clone();
 
                 tokio::spawn(async move {
                     match event {
-                        Ok(event) => event_handler.handle(cx, event).await,
+                        Ok(event) => event_handler.handle(context, event).await,
                         Err(err) => event_handler.error(err).await,
                     }
                 });
@@ -62,7 +56,7 @@ impl<T: EventHandler> Client<T> {
     async fn authenticate(&mut self) -> Result {
         self.ws_client
             .send(ClientToServerEvent::Authenticate {
-                token: self.token.clone(),
+                token: self.context.token(),
             })
             .await?;
 
--- src/client/context.rs
+++ src/client/context.rs
@@ -6,12 +6,19 @@ use crate::{http::HttpClient, models::User, Result};
 #[derive(Debug, Clone)]
 pub struct Context {
     /// A http client.
-    pub http_client: Arc<HttpClient>,
+    pub http_client: HttpClient,
+    token: Arc<String>,
 }
 
 impl Context {
-    pub(crate) fn new(http_client: Arc<HttpClient>) -> Self {
-        Self { http_client }
+    pub(crate) fn new(token: impl Into<String>) -> Self {
+        let token = token.into();
+        let http_client = HttpClient::new(&token);
+
+        Self {
+            http_client,
+            token: Arc::new(token),
+        }
     }
 
     /// Returns the current user.
@@ -19,5 +26,10 @@ impl Context {
         let user = self.http_client.get("users/@me").await?;
 
         Ok(user)
+    }
+
+    /// Returns the given token.
+    pub fn token(&self) -> String {
+        self.token.as_ref().clone()
     }
 }
