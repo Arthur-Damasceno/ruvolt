3e8a1ed21fb058195768a7bc6ceb29ac5b3c9dfd
Arthur Damasceno <damascenoarthur653@gmail.com>
2022-03-20 11:46:09 -0300

    docs(examples): remade ping_pong

--- examples/ping_pong.rs
+++ examples/ping_pong.rs
@@ -1,7 +1,7 @@
 use {
     async_trait::async_trait,
     ruvolt::{
-        models::{events::ReadyEvent, Message},
+        models::{events::ReadyEvent, Message, User},
         Client, Context, EventHandler, Result,
     },
     std::{env, time::Instant},
@@ -13,24 +13,33 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn ready(&self, cx: Context, _: ReadyEvent) {
-        let user = cx.user().await.unwrap();
-        println!("{} is ready!", user.username);
+        if let Ok(User { username, .. }) = cx.user().await {
+            println!("@{username} is ready!");
+        }
     }
 
     async fn message(&self, cx: Context, msg: Message) {
         let content = msg.content.to_string();
 
         if content == "!ping" {
-            let now = Instant::now();
-            let mut msg = msg.reply(&cx, "Pong!", true).await.unwrap();
+            let start = Instant::now();
 
-            let latency = (Instant::now() - now).as_millis();
-            let content = format!("Pong! The API latency is {}ms", latency);
+            if let Ok(mut msg) = msg.reply(&cx, "Pong!", true).await {
+                let delta_latency = (Instant::now() - start).as_millis();
+                let bonfire_latency = cx.latency().await.as_millis();
 
-            msg.edit(&cx, content).await.unwrap();
+                let content = format!(
+                    "### Pong!\
+                    \n#### The Delta API latency is `{delta_latency}ms`\
+                    \n#### The Bonfire API latency is `{bonfire_latency}ms`"
+                );
 
-            sleep(Duration::from_secs(3)).await;
-            msg.delete(&cx).await.unwrap();
+                msg.edit(&cx, content).await.ok();
+
+                sleep(Duration::from_secs(5)).await;
+
+                msg.delete(&cx).await.ok();
+            }
         }
     }
 }
@@ -40,9 +49,5 @@ async fn main() -> Result {
     let token = env::var("TOKEN").unwrap();
     let mut client = Client::new(Handler, token).await?;
 
-    if let Err(err) = client.listen().await {
-        eprintln!("{}", err);
-    }
-
-    Ok(())
+    client.listen().await
 }
